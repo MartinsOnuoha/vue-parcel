7aeb980329109d3112d155a6cb9270bb38057cef
MartinsOnuoha <martinsvictor.nd@gmail.com>
2020-03-13 08:27:08 +0100

    feat(home): sets up basic design for home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,33 @@
 <template>
-  <div>
-    WELCOME
+  <div class="row mt-5">
+    <div class="col-md-4" v-for="(household, i) in householdData" :key="i">
+      <HouseHold :household=household />
+    </div>
   </div>
 </template>
 
 <script>
+import HouseHold from '../components/HouseHold.vue';
+
 export default {
-  name: 'home'
-}
+  name: 'home',
+  data() {
+    return {
+      info: 'Welcome',
+      householdData: [
+        {
+          title: 'Card Title',
+          body: 'card body details'
+        },
+        {
+          title: 'Card Title',
+          body: 'card body details'
+        }
+      ]
+    }
+  },
+  components: {
+    HouseHold,
+  },
+};
 </script>
