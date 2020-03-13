54c33f740da829d102a75d66647dc0a3c668f58b
MartinsOnuoha <martinsvictor.nd@gmail.com>
2020-03-13 10:17:20 +0100

    feat(search): design for search bar component

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 import App from '../App.vue';
+import Searchbar from '../components/SearchBar.vue';
 
 new Vue({
   el: '#app',
   render: h => h(App),
 });
+
+// Vue.component('app-searchbar', Searchbar);
